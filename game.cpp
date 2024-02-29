7a78a71111560fa802ee1aa41daf279b84601253
Anton Shein <apsh14@bk.ru>
2024-03-01 00:45:59 +0300

    Add some sprite.

--- game.cpp
+++ game.cpp
@@ -1,8 +1,7 @@
 #include "game.hpp"
 
 CGame::CGame()
-    : mWindow(sf::VideoMode(640, 480), "SFML Application")
-    , mPlayer()
+    : mWindow(sf::VideoMode(640, 480), "SFML Application"), mPlayer()
 {
     mPlayer.setRadius(40.f);
     mPlayer.setPosition(100.f, 100.f);
@@ -11,10 +10,18 @@ CGame::CGame()
 
 void CGame::run()
 {
+    sf::Clock clock;
+    sf::Time timeSinceLastUpdate = sf::Time::Zero;
     while (mWindow.isOpen())
     {
         processEvents();
-        update();
+        timeSinceLastUpdate += clock.restart();
+        while (timeSinceLastUpdate > TimePerFrame)
+        {
+            timeSinceLastUpdate -= TimePerFrame;
+            processEvents();
+            update(TimePerFrame);
+        }
         render();
     }
 }
@@ -24,18 +31,57 @@ void CGame::processEvents()
     sf::Event event;
     while (mWindow.pollEvent(event))
     {
-        if (event.type == sf::Event::Closed)
-        mWindow.close();
+        switch (event.type)
+        {
+        case sf::Event::KeyPressed:
+            handlePlayerInput(event.key.code, true);
+            break;
+        case sf::Event::KeyReleased:
+            handlePlayerInput(event.key.code, false);
+            break;
+        case sf::Event::Closed:
+            mWindow.close();
+            break;
+        }
     }
 }
 
-void CGame::update()
+void CGame::update(sf::Time deltaTime)
 {
+    // sf::Vector2f movement(0.f, 0.f);
+    // if (mIsMovingUp)
+    //     movement.y -= PlayerSpeed;
+    // if (mIsMovingDown)
+    //     movement.y += PlayerSpeed;
+    // if (mIsMovingLeft)
+    //     movement.x -= PlayerSpeed;
+    // if (mIsMovingRight)
+    //     movement.x += PlayerSpeed;
+    // mPlayer.move(movement * deltaTime.asSeconds());
 }
 
 void CGame::render()
 {
+    sf::Texture texture;
+    if (!texture.loadFromFile("/home/shap/Projects/cpp-projects/pilot-game/adventurer-idle-00.png"))
+    {
+        // Handle loading error
+    }
+    sf::Sprite sprite(texture);
+    sprite.setPosition(100.f, 100.f);
     mWindow.clear();
-    mWindow.draw(mPlayer);
+    mWindow.draw(sprite);
     mWindow.display();
+}
+
+void CGame::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+    // if (key == sf::Keyboard::W)
+    //     mIsMovingUp = isPressed;
+    // else if (key == sf::Keyboard::S)
+    //     mIsMovingDown = isPressed;
+    // else if (key == sf::Keyboard::A)
+    //     mIsMovingLeft = isPressed;
+    // else if (key == sf::Keyboard::D)
+    //     mIsMovingRight = isPressed;
 }
--- game.hpp
+++ game.hpp
@@ -15,10 +15,13 @@ private:
     
     sf::RenderWindow mWindow;
     sf::CircleShape  mPlayer;
+    const sf::Time TimePerFrame = sf::seconds(1.f / 60.f);
 
     void processEvents();
-    void update();
+    void update(sf::Time deltaTime);
     void render();
+
+    void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
 };
 
 #endif // CGAME_H
